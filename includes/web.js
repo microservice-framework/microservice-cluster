b0cc319325712075ba2eacfa386848a2205dfe78
Gor Martsen <Gor.martsen@gmail.com>
2016-07-14 15:23:54 -0400

    Add data processing prototype

--- includes/web.js
+++ includes/web.js
@@ -37,33 +37,39 @@ WebServer.prototype.RequestHandler = function( request, response ) {
   var self = this;
   self.debug.request( "%s: %s", request.method, request.url );
   var data = "";
-  try {
-    switch ( request.method ) {
-      case "POST":
-          throw new Error( "POST" );
-        break;
-      case "GET":
-          throw new Error( "GET" );
-        break;
-      case "PUT":
-          throw new Error( "PUT" );
-        break;
-      case "DELETE":
-          throw new Error( "DELETE" );
-        break;
-      case "PATCH":
-          throw new Error( "PATCH" );
-        break;
-      default:
-          throw new Error( "UNKNOW" );
+  request.addListener( "data", function( chunk ) { data += chunk; } );
+  request.addListener( "end", function( ) {
+    if(data) {
+      self.debug.request( "Data: %s", data );
     }
-    response.writeHead( 500, { "content-type": "application/json" } );
-    response.end( "\n" );
-  } catch ( e ) {
-    response.writeHead( 500, { "content-type": "application/json" } );
-    response.write( JSON.stringify( { "error": "Internal error" }, null, 2 ) );
-    response.end( "\n" );
-    self.debug.request( "Error intersepted:\n %s", e.stack );
+    try {
+      switch ( request.method ) {
+        case "POST":
+            throw new Error( "POST" );
+          break;
+        case "GET":
+            throw new Error( "GET" );
+          break;
+        case "PUT":
+            throw new Error( "PUT" );
+          break;
+        case "DELETE":
+            throw new Error( "DELETE" );
+          break;
+        case "PATCH":
+            throw new Error( "PATCH" );
+          break;
+        default:
+            throw new Error( "UNKNOW" );
+      }
+      response.writeHead( 500, { "content-type": "application/json" } );
+      response.end( "\n" );
+    } catch ( e ) {
+      response.writeHead( 500, { "content-type": "application/json" } );
+      response.write( JSON.stringify( { "error": "Internal error" }, null, 2 ) );
+      response.end( "\n" );
+      self.debug.request( "Error intersepted:\n %s", e.stack );
+    }
   }
 };
 
