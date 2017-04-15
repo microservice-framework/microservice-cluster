4ac9cf1f2642438506d47611bc6f0456fee48c29
Gor Martsen <Gor.martsen@gmail.com>
2017-04-15 12:16:41 -0400

    Code clean up

--- includes/web.js
+++ includes/web.js
@@ -1,6 +1,6 @@
 /**
  * Handle HTTP startup and shutdown.
- * Parse request and s
+ * Parse request and send answer.
  */
 'use strict';
 
@@ -25,12 +25,12 @@ function WebServer(data) {
   self.server = http.createServer(self.RequestHandler);
 
   // Use random port if port settings is not provided.
-  if(!self.data.port) {
+  if (!self.data.port) {
     self.data.port = 0;
   }
 
   // Use address if provided.
-  if(self.data.hostname) {
+  if (self.data.hostname) {
     self.server.listen(self.data.port, self.data.hostname);
   } else {
     self.server.listen(self.data.port);
@@ -52,7 +52,7 @@ WebServer.prototype.server = false;
 WebServer.prototype.RequestHandler = function(request, response) {
   var self = this;
 
-  self.debug.log('%s: %s', request.method, request.url);
+  self.debug.log('Request: %s: %s', request.method, request.url);
   var _buffer = '';
   var data = '';
 
@@ -70,13 +70,13 @@ WebServer.prototype.RequestHandler = function(request, response) {
       try {
         data = JSON.parse(_buffer);
       } catch (e) {
-        if (self.data.callbacks["responseHandler"]) {
-          return self.data.callbacks["responseHandler"](e, null, response, requestDetails);
+        if (self.data.callbacks['responseHandler']) {
+          return self.data.callbacks['responseHandler'](e, null, response, requestDetails);
         }
         response.writeHead(500, { 'content-type': 'application/json' });
         response.write(JSON.stringify({ error: e.message }, null, 2));
         response.end('\n');
-        self.debug.debug('Error intersepted:\n %s', e.stack);
+        self.debug.debug('Error catched:\n %s', e.stack);
         return;
       }
     } else {
@@ -85,14 +85,12 @@ WebServer.prototype.RequestHandler = function(request, response) {
 
     if (self.data.callbacks.validate) {
       self.data.callbacks.validate(request.method, _buffer, requestDetails, function(err) {
-        if (!err) {
-          self.RequestProcess(request.method, response, requestDetails, data);
-        } else {
-          if(!err.code) {
+        if (err) {
+          if (!err.code) {
             err.code = 403;
           }
-          if (self.data.callbacks["responseHandler"]) {
-            return self.data.callbacks["responseHandler"](err, null, response, requestDetails);
+          if (self.data.callbacks['responseHandler']) {
+            return self.data.callbacks['responseHandler'](err, null, response, requestDetails);
           }
           response.writeHead(err.code, { 'content-type': 'application/json' });
           response.write(JSON.stringify({ message: err.message }, null, 2));
@@ -100,10 +98,11 @@ WebServer.prototype.RequestHandler = function(request, response) {
           self.debug.debug('Validation error: %s', err.message);
           return;
         }
-      })
-    } else {
-      self.RequestProcess(request.method, response, requestDetails, data);
+        return self.RequestProcess(request.method, response, requestDetails, data);
+      });
+      return;
     }
+    return self.RequestProcess(request.method, response, requestDetails, data);
   });
 };
 
@@ -113,21 +112,21 @@ WebServer.prototype.RequestHandler = function(request, response) {
 WebServer.prototype.RequestProcess = function(method, response, requestDetails, data) {
   var self = this;
 
-  self.debug.debug('Parsed data: %s', JSON.stringify(data, null, 2));
+  self.debug.debug('Parsed data: %O', data);
   try {
     if (self.data.callbacks[method]) {
       self.data.callbacks[method](data, requestDetails, function(err, handlerResponse) {
         self.callbackExecutor(err, handlerResponse, response, requestDetails);
       });
     } else {
-      throw new Error('Do not support.');
+      throw new Error(method + ' is not supported.');
     }
   } catch (e) {
     self.debug.debug('Error intersepted:\n %s', e.stack);
     e.code = 500;
 
-    if (self.data.callbacks["responseHandler"]) {
-      return self.data.callbacks["responseHandler"](e, null, response, requestDetails);
+    if (self.data.callbacks['responseHandler']) {
+      return self.data.callbacks['responseHandler'](e, null, response, requestDetails);
     }
     response.writeHead(e.code, { 'content-type': 'application/json' });
     response.write(JSON.stringify({ error: e.message }, null, 2));
@@ -141,25 +140,25 @@ WebServer.prototype.RequestProcess = function(method, response, requestDetails, 
 WebServer.prototype.callbackExecutor = function(err, handlerResponse, response, requestDetails) {
   var self = this;
 
-  if (self.data.callbacks["responseHandler"]) {
-    return self.data.callbacks["responseHandler"](err, handlerResponse, response, requestDetails);
+  if (self.data.callbacks['responseHandler']) {
+    return self.data.callbacks['responseHandler'](err, handlerResponse, response, requestDetails);
   }
 
   if (err) {
-    self.debug.debug('Handler responce error:\n %s', JSON.stringify(err , null, 2));
+    self.debug.debug('Handler responce error:\n %O', err);
     response.writeHead(503, { 'content-type': 'application/json' });
     response.write(JSON.stringify({message: err.message }, null, 2));
     response.end('\n');
   }else {
-    self.debug.debug('Handler responce:\n %s', JSON.stringify(handlerResponse , null, 2));
-    if(handlerResponse.headers) {
-      if(!handlerResponse.headers['content-type']) {
+    self.debug.debug('Handler responce:\n %O', handlerResponse);
+    if (handlerResponse.headers) {
+      if (!handlerResponse.headers['content-type']) {
         handlerResponse.headers['content-type'] = 'application/json';
       }
     } else {
       handlerResponse.headers = { 'content-type': 'application/json' };
     }
-    response.writeHead(handlerResponse.code, handlerResponse.headers );
+    response.writeHead(handlerResponse.code, handlerResponse.headers);
     response.write(JSON.stringify(handlerResponse.answer , null, 2));
     response.end('\n');
   }
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
- * Central Startup file.
  * Launch cluster and workers.
  * React on SIGINT and SIGTERM.
  * restart worker if worker exit.
  */
+
 'use strict';
 
 const cluster = require('cluster');
@@ -31,21 +31,24 @@ function Cluster(data) {
     } else {
       numCPUs = require('os').cpus().length;
     }
+
     self.debug.log('Starting up %s workers.', numCPUs);
     for (var i = 0; i < numCPUs; i++) {
       cluster.fork();
     }
+
     cluster.on('online', function(worker) {
       self.debug.log('Worker %s is online', worker.process.pid);
     });
+
     cluster.on('exit', function(worker, code, signal) {
       self.debug.log('Worker %s died. code %s signal %s', worker.process.pid, code, signal);
       self.debug.log('Starting a new worker');
       cluster.fork();
     });
 
-    cluster.on('listening', function(worker, address){
-      if(self.data.callbacks['init']) {
+    cluster.on('listening', function(worker, address) {
+      if (self.data.callbacks['init']) {
         self.data.callbacks['init'](cluster, worker, address);
       }
     });
