f4a36e4d2b9c0fc14c40bcc559a2bceae2f07088
Gor Martsen <Gor.martsen@gmail.com>
2025-02-23 01:23:29 -0500

    testing code

--- test/simple.js
+++ test/simple.js
@@ -8,10 +8,15 @@
 
 import Cluster from '../index.js';
 
-var mcluster = new Cluster({
+var ms = new Cluster({
   port: 10000,
-  count: 3,
+  count: 1,
   pid: './cluster.pid',
+  loader: function (request, callback) {
+    console.log('loader', request.url);
+    request.test = true;
+    callback(null);
+  },
   singleton: function (isStart, variables) {
     console.log('singleton', isStart, variables);
     if (isStart) {
@@ -22,41 +27,60 @@ var mcluster = new Cluster({
     callback({ test: 1 });
     console.log('init');
   },
+  /*responseHandler: function(error, handlerResponse, response, request) {
+    console.log('responseHandler', error, handlerResponse);
+    if(error) {
+      if (!err.code) {
+        err.code = 503;
+      }
+      response.writeHead(e.code, () => {
+        return this.validateHeaders({});
+      });
+      response.write(JSON.stringify({ error: e.message }, null, 2));
+      response.end('\n');
+      return
+    }
+
+    response.writeHead(handlerResponse.code, {'content-type': 'application/json'});
+    response.write(JSON.stringify(handlerResponse.answer));
+    response.end('\n');
+  },*/
   methods: {
-    POST: function (data, requestDetails, callback) {
-      console.log('post %O %O', data, requestDetails);
+    POST: function (data, request, callback) {
+      console.log('post %O %O', data, request);
       callback(null, {
         code: 200,
         answer: { test: 1, data: data },
         headers: { test: 100 },
       });
     },
-    GET: function (data, requestDetails, callback) {
-      console.log('post %O %O', data, requestDetails);
+    GET: function (data, request, callback) {
+      console.log('post %O %O', data, request);
       callback(null, {
         code: 200,
         answer: { test: 1, data: data },
         headers: { test: 100 },
       });
     },
-    PUT: function (data, requestDetails, callback) {
-      console.log('post %O %O', data, requestDetails);
+    PUT: function (data, request, callback) {
+      console.log('post %O %O', data, request);
       callback(null, {
         code: 200,
         answer: { test: 1, data: data },
         headers: { test: 100 },
       });
     },
-    DELETE: function (data, requestDetails, callback) {
-      console.log('post %O %O', data, requestDetails);
+    DELETE: function (data, request, callback) {
+      //console.log('post %O %O', data, request);
+      return callback(new Error('test'));
       callback(null, {
         code: 200,
         answer: { test: 1, data: data },
         headers: { test: 100 },
       });
     },
-    SEARCH: function (data, requestDetails, callback) {
-      console.log('post %O %O', data, requestDetails);
+    SEARCH: function (data, request, callback) {
+      console.log('post %O %O', data, request);
       callback(null, {
         code: 200,
         answer: { test: 1, data: data },
@@ -64,12 +88,14 @@ var mcluster = new Cluster({
       });
     },
   },
-})
-  .on('online', function (worker) {
-    console.log('Worker %s is online', worker.process.pid);
-  })
-  .on('exit', function (worker, code, signal) {
-    console.log('Worker %s died. code %s signal %s', worker.process.pid, code, signal);
-  });
+});
+
+ms.on('online', function (worker) {
+  console.log('Worker %s is online', worker.process.pid);
+});
+
+ms.on('exit', function (worker, code, signal) {
+  console.log('Worker %s died. code %s signal %s', worker.process.pid, code, signal);
+});
 
 console.log('mcluster:!');
